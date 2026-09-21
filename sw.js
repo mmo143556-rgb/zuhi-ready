7a8ed3ec47fe83961e4d310bc66d9b6e2f74f58b
mmo143556-rgb <mmo143556@gmail.com>
2026-09-21 22:45:13 +0300

    Refresh application cache

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'zuhi-shell-v4';
-const SHELL = ['./', './index.html', './assets/style.css', './js/app.js', './config.js', './manifest.webmanifest', './assets/brand-board.png'];
+const CACHE = 'zuhi-shell-v5';
+const SHELL = ['./', './index.html', './assets/style.css', './js/app.js', './config.js', './manifest.webmanifest', './assets/brand-board.png', './admin/index.html', './js/admin.js'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL))));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))));
 self.addEventListener('fetch', event => {
