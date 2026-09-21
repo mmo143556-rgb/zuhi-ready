5369eca450d00964c15fe160c1cb1a4783aa9c0f
mmo143556-rgb <mmo143556@gmail.com>
2026-09-21 22:30:24 +0300

    Refresh PWA cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'zuhi-shell-v3';
+const CACHE = 'zuhi-shell-v4';
 const SHELL = ['./', './index.html', './assets/style.css', './js/app.js', './config.js', './manifest.webmanifest', './assets/brand-board.png'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL))));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))));
